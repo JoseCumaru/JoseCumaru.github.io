e7c3305b2e8eb2d2199a4bb60b936e035531c108
JoseCumaru <105247629+JoseCumaru@users.noreply.github.com>
2022-12-16 20:33:29 -0400

    commit inicial
    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider-principal').slick({
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: true,
+        infinite: true,
+        speed: 900,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+  });
